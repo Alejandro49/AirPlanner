d15921287baa5573a8c19ad60e49998264dd4a05
Alejandro49 <alejandrollorente6@gmail.com>
2021-01-12 21:49:10 +0100

    nada importante

--- AirPlannerApp/src/principal/AnadirVueloServlet.java
+++ AirPlannerApp/src/principal/AnadirVueloServlet.java
@@ -29,17 +29,13 @@ public class AnadirVueloServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		 int idVueloDeseado = Integer.parseInt(request.getParameter("OPCION"));
